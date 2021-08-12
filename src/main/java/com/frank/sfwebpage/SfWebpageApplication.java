d02e528af09e3e197966183ff74c8fca12216272
Francisco-GV <francisco_gasca@hotmail.com>
2021-08-12 17:27:20 -0500

    Initial Project Add

---
+++ src/main/java/com/frank/sfwebpage/SfWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.frank.sfwebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfWebpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frank/sfwebpage/SfWebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.sfwebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfWebpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
