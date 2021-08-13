6a571316c68f33ed9d8919f8c6a602d6118893d7
Francisco-GV <francisco_gasca@hotmail.com>
2021-08-12 20:03:43 -0500

    Adding base entity. Closes #9.

--- webpage-data/src/main/java/com/frank/sfwebpage/model/Person.java
+++ webpage-data/src/main/java/com/frank/sfwebpage/model/Person.java
@@ -1,6 +1,6 @@
 package com.frank.sfwebpage.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- webpage-data/src/main/java/com/frank/sfwebpage/model/PetType.java
+++ webpage-data/src/main/java/com/frank/sfwebpage/model/PetType.java
@@ -1,6 +1,6 @@
 package com.frank.sfwebpage.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
