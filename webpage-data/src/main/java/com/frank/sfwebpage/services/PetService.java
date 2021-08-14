fab32e61169f3673245541a9201f1823e5927409
Francisco-GV <francisco_gasca@hotmail.com>
2021-08-13 19:01:21 -0500

    Refactoring to common CRUD interface. Closes #10.

--- webpage-data/src/main/java/com/frank/sfwebpage/services/PetService.java
+++ webpage-data/src/main/java/com/frank/sfwebpage/services/PetService.java
@@ -2,11 +2,5 @@ package com.frank.sfwebpage.services;
 
 import com.frank.sfwebpage.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findByLastName(String lastName);
-    Pet findById(Long id);
-    Pet save (Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
