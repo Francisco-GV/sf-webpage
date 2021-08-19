ce82c3147e518195b41cd4f468f81e45e88e1e9e
Francisco-GV <francisco_gasca@hotmail.com>
2021-08-19 01:19:31 -0500

    Refactored Map Implementation to abstract ID generation. Closes #22.

--- webpage-data/src/main/java/com/frank/sfwebpage/services/map/AbstractMapService.java
+++ webpage-data/src/main/java/com/frank/sfwebpage/services/map/AbstractMapService.java
@@ -1,13 +1,16 @@
 package com.frank.sfwebpage.services.map;
 
+import com.frank.sfwebpage.model.BaseEntity;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     public Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +20,16 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    public T save(ID id, T object) {
-        map.put(id, object);
+    public T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(), object);
+        } else {
+            throw new NullPointerException("Object cannot be null!!!");
+        }
 
         return object;
     }
@@ -29,5 +40,11 @@ public abstract class AbstractMapService<T, ID> {
 
     public void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        return (!map.isEmpty())
+                ? Collections.max(map.keySet()) + 1
+                : 1L;
     }
 }
