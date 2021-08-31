0fabf780bf90ff6e65bedef0e1f98c43d68e7122
Francisco-GV <francisco_gasca@hotmail.com>
2021-08-30 21:27:23 -0500

    Added Specialty Entity, updated Vet with Specialty relationship. Closes #26.

---
+++ webpage-data/src/main/java/com/frank/sfwebpage/model/Specialty.java
@@ -0,0 +1,14 @@
+package com.frank.sfwebpage.model;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
