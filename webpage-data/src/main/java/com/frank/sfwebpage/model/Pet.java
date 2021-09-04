361300b65f0816f22bb7e7f0707e0686e7de1d9f
Francisco-GV <francisco_gasca@hotmail.com>
2021-09-04 14:28:27 -0500

    Updated Owner with JPA entity mappings. Closes #35.

--- webpage-data/src/main/java/com/frank/sfwebpage/model/Pet.java
+++ webpage-data/src/main/java/com/frank/sfwebpage/model/Pet.java
@@ -1,12 +1,27 @@
 package com.frank.sfwebpage.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pet")
 public class Pet extends BaseEntity {
 
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public String getName() {
