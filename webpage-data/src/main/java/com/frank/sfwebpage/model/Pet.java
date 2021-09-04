d0a4c79845e4e5e073bbe7526f3ee4656f5cd547
Francisco-GV <francisco_gasca@hotmail.com>
2021-09-04 16:56:31 -0500

    Added Visit Entity. Closes #37.

--- webpage-data/src/main/java/com/frank/sfwebpage/model/Pet.java
+++ webpage-data/src/main/java/com/frank/sfwebpage/model/Pet.java
@@ -1,11 +1,15 @@
 package com.frank.sfwebpage.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pet")
@@ -23,6 +27,9 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -54,5 +61,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
