11f26c3ec9cfeca760f89df953b505da9bd705c0
Francisco-GV <francisco_gasca@hotmail.com>
2021-09-04 19:34:14 -0500

    Added JPA Service for Vet. Closes #40.

---
+++ webpage-data/src/main/java/com/frank/sfwebpage/services/jpa/VetServiceJpa.java
@@ -0,0 +1,52 @@
+package com.frank.sfwebpage.services.jpa;
+
+import com.frank.sfwebpage.model.Vet;
+import com.frank.sfwebpage.repositories.VetRepository;
+import com.frank.sfwebpage.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class VetServiceJpa implements VetService {
+
+    private final VetRepository vetRepository;
+
+    @Autowired
+    public VetServiceJpa(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+
+        vetRepository.findAll().forEach(vets::add);
+
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
