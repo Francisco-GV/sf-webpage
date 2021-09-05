aa4a8796aa91652988c154ff1e511859adc22db8
Francisco-GV <francisco_gasca@hotmail.com>
2021-09-04 19:34:00 -0500

    Minor change.

--- webpage-data/src/main/java/com/frank/sfwebpage/services/jpa/OwnerServiceJpa.java
+++ webpage-data/src/main/java/com/frank/sfwebpage/services/jpa/OwnerServiceJpa.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
-@Profile({"jpa", "default"})
+@Profile("jpa")
 public class OwnerServiceJpa implements OwnerService {
 
     private final OwnerRepository ownerRepository;
