104e1e3ee50934df972b4ce6e51d9ada03c05769
Francisco-GV <francisco_gasca@hotmail.com>
2021-09-04 19:47:00 -0500

    Added JPA Service for Pet. Closes #42.

---
+++ webpage-data/src/main/java/com/frank/sfwebpage/services/jpa/PetServiceJpa.java
@@ -0,0 +1,52 @@
+package com.frank.sfwebpage.services.jpa;
+
+import com.frank.sfwebpage.model.Pet;
+import com.frank.sfwebpage.repositories.PetRepository;
+import com.frank.sfwebpage.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetServiceJpa implements PetService {
+
+    private final PetRepository petRepository;
+
+    @Autowired
+    public PetServiceJpa(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+
+        petRepository.findAll().forEach(pets::add);
+
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
