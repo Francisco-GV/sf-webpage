192b69cd77d277673ee523c2002dd722483aeffe
Francisco-GV <francisco_gasca@hotmail.com>
2021-09-04 20:33:17 -0500

    Added JPA implementation for VisitService. Closes #45.

---
+++ webpage-data/src/main/java/com/frank/sfwebpage/services/jpa/VisitJpaService.java
@@ -0,0 +1,51 @@
+package com.frank.sfwebpage.services.jpa;
+
+import com.frank.sfwebpage.model.Visit;
+import com.frank.sfwebpage.repositories.VisitRepository;
+import com.frank.sfwebpage.services.VisitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class VisitJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    @Autowired
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+
+        visitRepository.findAll().forEach(visits::add);
+
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
