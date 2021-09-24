b6fc985273ef3a66cb0dcb9d1a10f2fdb31d0abf
Francisco-GV <francisco_gasca@hotmail.com>
2021-09-24 06:32:59 -0500

    Added JSON endpoint. Closes #63.

--- webpage-web/src/main/java/com/frank/sfwebpage/controllers/VetController.java
+++ webpage-web/src/main/java/com/frank/sfwebpage/controllers/VetController.java
@@ -1,10 +1,15 @@
 package com.frank.sfwebpage.controllers;
 
+import com.frank.sfwebpage.model.Vet;
 import com.frank.sfwebpage.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/vets")
@@ -17,7 +22,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @GetMapping({"", "/", "/index", "/index.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
@@ -25,9 +30,14 @@ public class VetController {
         return "vets/vetList";
     }
 
-    @RequestMapping({"/find", "/find.html"})
+    @GetMapping({"/find", "/find.html"})
     public String findVet(Model model) {
         model.addAttribute("message", "Not implemented yet!!!");
         return "error";
+    }
+
+    @GetMapping("/api")
+    public @ResponseBody Set<Vet> listVetsJson() {
+        return vetService.findAll();
     }
 }
